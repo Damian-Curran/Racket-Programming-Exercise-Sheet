efc262c6a67a7e9a62c0fc8ac2a372bfb2e0dbf8
Damian404 <sgt,curren@gmail.com>
2018-04-08 10:56:23 +0100

    third exercise, rcycle added

--- lcycle-rcycle.rkt
+++ lcycle-rcycle.rkt
@@ -9,3 +9,15 @@
         (else (helper (cdr (cdr x)) (cons (car x) '()) (cons(car (cdr x)) '())))))
 
 (lcycle (list 1 2 3 4 5))
+
+
+(define (rcycle x)
+  (define (helper x k y)
+    (cond ((null? y) (cons (car x) k))
+          ((null? (cdr x)) (helper x (cons (car y) k) (cdr y)))
+          (else (helper (cdr x) k (cons (car x) y)))))
+  (cond ((null? (cdr x)) x)
+        (else (helper (cdr x) '() (cons (car x) '())))))
+
+
+(rcycle (list 1 2 3 4 5))
